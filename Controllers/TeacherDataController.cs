b6bc4083dedafbee5879b16b83bfd91676cce0e5
Haritha-Kotapati <n01548934@humber.ca>
2023-03-21 18:19:04 -0400

    Add project files.

---
+++ Controllers/TeacherDataController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SchoolProject.Models;
+using MySql.Data.MySqlClient;
+
+namespace SchoolProject.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        //The database context class which llows us to access our MySQL Database.
+        private SchoolDbContext School = new SchoolDbContext();
+
+        ///This Controller will access the teachers table of our School database.
+        ///<summary>
+        ///Returns a list of Teachers in the system.
+        /// </summary>
+        /// <example>GET api/TeacherData/ListTeachers</example>
+        /// <returns>
+        /// A list of teachers (first names and last names) 
+        /// </returns>
+        /// 
+        [HttpGet]
+        public IEnumerable<Teacher> ListTeachers()
+        {
+            //Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from Teachers";
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            //Create an empty list of Teacher Names
+            List<Teacher> Teachers = new List<Teacher>{};
+
+            //Loop through each row the Result Set
+            while (ResultSet.Read())
+            {
+                //Access column information by the DB column name as an index
+                int TeacherId = (int)ResultSet["teacherid"];
+                string TeacherFname = (string)ResultSet["teacherfname"];
+                string TeacherLname = (string)ResultSet["teacherlname"];
+                string EmployeeNumber  =  (string)ResultSet["employeenumber"];
+                DateTime HireDate = (DateTime)ResultSet["hiredate"];
+                decimal Salary = (decimal)ResultSet["salary"];
+
+                Teacher NewTeacher = new Teacher();
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmployeeNumber;
+               // NewTeacher.HireDate = HireDate;
+               // NewTeacher.Salary = Salary;
+
+                //Add the Teacher Name to the list
+                Teachers.Add(NewTeacher);
+            }
+
+            //Close the connection between the MYSQL Database and the WebServer
+            Conn.Close();
+
+            //Return the final list of teachers names
+            return Teachers;
+
+        }
+
+        [HttpGet]
+        /// <example>GET api/TeacherData/FindTeacher/4</example>
+        public Teacher FindTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            //Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from Teachers where teacherid = " + id;
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                int TeacherId = (int)ResultSet["teacherid"];
+                string TeacherFname = (string)ResultSet["teacherfname"];
+                string TeacherLname = (string)ResultSet["teacherlname"];
+                string EmployeeNumber = (string)ResultSet["employeenumber"];
+                DateTime HireDate = (DateTime)ResultSet["hiredate"];
+                decimal Salary = (decimal)ResultSet["salary"];
+
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmployeeNumber;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+            }
+
+            return NewTeacher;
+        }
+
+    }
+
+}
